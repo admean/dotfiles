0480814feb9d2d6c581c96f2c8b4c062a5f916e2
Mateusz Jarosz <m.jarosz@admean.pl>
2016-04-09 21:00:54 +0200

    add zsh plugins

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,8 +1,8 @@
 # ZSH aliases
 
-alias zshconfig="vim ~/.zshrc"
-alias aliasconfig="vim ~/.oh-my-zsh/custom/aliases.zsh"
-alias powerlevel9kconfig="vim ~/.oh-my-zsh/custom/powerlevel9k.zsh"
-alias home="cd ~"
+alias zshconf="vim ~/.zshrc"
+alias alc="vim ~/.oh-my-zsh/custom/aliases.zsh"
+alias pl9c="vim ~/.oh-my-zsh/custom/powerlevel9k.zsh"
+alias gh="cd ~"
 alias please='sudo'
 alias fuck='sudo $(history -p \!\!)'
--- .zshrc
+++ .zshrc
@@ -50,7 +50,7 @@ HIST_STAMPS="dd.mm.yyyy"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(dircycle gem git rails ruby rvm tmux)
 
 # User configuration
 
